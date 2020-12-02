029948e81aff099f4006be54870d69f2bd273517
graham <1731354527@qq.com>
2020-12-02 15:49:23 +0800

    169 多数元素 HashMap

---
+++ leetcode/editor/cn/[169]多数元素.java
@@ -0,0 +1,55 @@
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。 
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [3,2,3]
+//输出: 3 
+//
+// 示例 2: 
+//
+// 输入: [2,2,1,1,1,2,2]
+//输出: 2
+// 
+// Related Topics 位运算 数组 分治算法 
+// 👍 809 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //第一种方法 HashMap
+    //创建自定义HashMap，key：数字值，value：出现次数
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            } else {
+                counts.put(num, counts.get(num) + 1);
+            }
+        }
+        return counts;
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+/*解题思路
+HashMap：穷举法，遇到的所有元素都加入Map，返回Map中 值最大的value。复杂度O(n).
+*/
